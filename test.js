a09dc6eb3e31c56b9a3ba53d228adc600fa4d374
Deeraj21 <162298598+Deeraj21@users.noreply.github.com>
2024-11-11 11:20:41 +0530

    Update test.js
    const api_key1="9c71b1f41a8e405fb0fdb0f3d45b178f7e4a1b33;
    const pass="P@ssw0rd123"
    const hex_password="4d3c2b2a1f8e9d7c6b5a4f3e2d1c0b9a";

--- test.js
+++ test.js
@@ -5,6 +5,10 @@ const accessKeyId = 'AKIAIOSFODNN7EXAMPLE';
 const secretAccessKey = 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY';
 
 const stripeApiKey = 'sk_test_4eC39HqLyjWDarjtT1zdp7dc';
+const api_key="d6F3Efeqw4Jh83dPl9Ak5nFZ2sUi7zY0;
+const api_key1="9c71b1f41a8e405fb0fdb0f3d45b178f7e4a1b33;
+const pass="P@ssw0rd123"
+const hex_password="4d3c2b2a1f8e9d7c6b5a4f3e2d1c0b9a";
 
 // GitHub token
 
